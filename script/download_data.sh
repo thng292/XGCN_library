1af342bc5b8534101f994b825773e4f4d29decac
thng292 <nguyenquangthong292@gmail.com>
2026-03-26 11:18:06 +0700

    Preparing my experiment, working good.

---
+++ script/download_data.sh
@@ -0,0 +1,4 @@
+wget -P XGCN_library/data/raw_yelp2018 -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/train.txt
+wget -P XGCN_library/data/raw_yelp2018 -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/test.txt
+wget -P XGCN_library/data/raw_gowalla -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/train.txt
+wget -P XGCN_library/data/raw_gowalla -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/test.txt
