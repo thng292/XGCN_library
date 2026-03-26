5ec2301c2ebe2f2c4c7ad2eb8b5e4317f8587746
thng292 <nguyenquangthong292@gmail.com>
2026-03-26 21:21:36 +0700

    Fix download script for older wget

--- script/download_data.sh
+++ script/download_data.sh
@@ -1,4 +1,4 @@
-wget -P XGCN_library/data/raw_yelp2018 -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/train.txt
-wget -P XGCN_library/data/raw_yelp2018 -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/test.txt
-wget -P XGCN_library/data/raw_gowalla -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/train.txt
-wget -P XGCN_library/data/raw_gowalla -4sS https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/test.txt
+wget -P XGCN_library/data/raw_yelp2018 -4S https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/train.txt
+wget -P XGCN_library/data/raw_yelp2018 -4S https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/yelp2018/test.txt
+wget -P XGCN_library/data/raw_gowalla -4S https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/train.txt
+wget -P XGCN_library/data/raw_gowalla -4S https://raw.githubusercontent.com/gusye1234/LightGCN-PyTorch/refs/heads/master/data/gowalla/test.txt
